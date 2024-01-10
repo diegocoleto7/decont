6667a5e853629da7ffbaa430f5cea8bed69a8d89
Diego Coleto Checa <diegocoleto7@gmail.com>
2024-01-10 14:15:31 +0100

    Add Download.sh script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,18 +1,27 @@
-# This script should download the file specified in the first argument ($1),
-# place it in the directory specified in the second argument ($2),
-# and *optionally*:
-# - uncompress the downloaded file with gunzip if the third
-#   argument ($3) contains the word "yes"
-# - filter the sequences based on a word contained in their header lines:
-#   sequences containing the specified word in their header should be **excluded**
-#
-# Example of the desired filtering:
-#
-#   > this is my sequence
-#   CACTATGGGAGGACATTATAC
-#   > this is my second sequence
-#   CACTATGGGAGGGAGAGGAGA
-#   > this is another sequence
-#   CCAGGATTTACAGACTTTAAA
-#
-#   If $4 == "another" only the **first two sequence** should be output
+
+# Argumentos enviados desde pipeline.sh
+url="$1"
+destination_directory="$2"
+extract="$3"
+
+# Descargar el archivo
+wget "$url" -P "$destination_directory"
+
+# Verificar si se debe descomprimir
+if [ "$extract" = "yes" ]; then
+    # Obtener el nombre del archivo descargado
+    filename=$(basename "$url")
+
+    # Descomprimir el archivo con gunzip
+    if [ "${filename##*.}" = "gz" ]; then
+        gunzip -k "$destination_directory/$filename"
+
+        # Filtrar y eliminar secuencias de snRNA con seqkit
+        seqkit grep -v -n -r -p "small nuclear" "$destination_directory/${filename%.*}" | seqkit grep -v -n -r -p "snRNA" > "$destination_directory/${filename%.*}_filtered.fasta"
+        
+        # Reemplazar el archivo original con el filtrado
+        mv "$destination_directory/${filename%.*}_filtered.fasta" "$destination_directory/${filename%.*}"
+    else
+        echo "Error: Format not supported for decompression."
+    fi
+fi
