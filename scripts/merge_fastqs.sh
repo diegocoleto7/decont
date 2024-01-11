1b69da76b0fa2912cafbab1e3317faa590154a11
Diego Coleto Checa <diegocoleto7@gmail.com>
2024-01-11 12:28:25 +0100

    Add merge_fastqs.sh script

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,5 +1,18 @@
-# This script should merge all files from a given sample (the sample id is
-# provided in the third argument ($3)) into a single file, which should be
-# stored in the output directory specified by the second argument ($2).
-#
-# The directory containing the samples is indicated by the first argument ($1).
+
+# Directorio de entrada
+input_dir="$1"
+
+# Directorio de salida
+output_dir="$2"
+
+# Identificador de la muestra
+sample_id="$3"
+
+# Crear el nombre del archivo de salida
+output_file="${output_dir}/${sample_id}.fastq.gz"
+
+# Concatenar los archivos de entrada
+cat "${input_dir}/${sample_id}.5dpp.1.1s_sRNA.fastq.gz" "${input_dir}/${sample_id}.5dpp.1.2s_sRNA.fastq.gz"  > "${output_file}"
+
+# Imprimir mensaje
+echo "Merged files in: ${output_file}"
